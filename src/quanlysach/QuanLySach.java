4dfa88d773ed8bdd461d210ed13e9c6e0a3b1699
Long Thank <longltph45498@pfpt.edu.vn>
2023-11-14 13:17:48 +0700

    first commit~

---
+++ src/quanlysach/QuanLySach.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quanlysach;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class QuanLySach {
+
+    ArrayList<Sach> list = new ArrayList<>();
+
+    public QuanLySach() {
+        list.add(new Sach("MS001", "TAY DU KY", "TIEU THUYET", "TG1","200000" ,"20"));
+        list.add(new Sach("MS002", "DPRAEMON", "TRUYEN", "TG2","100000" ,"10"));
+        list.add(new Sach("MS003", "THANH", "TRUYEN", "TG3", "100000","30"));
+        list.add(new Sach("MS004", "THAM LAM", "TRUYEN", "TG4","100000" ,"10"));
+        list.add(new Sach("MS005", "TU DUY", "TRUYEN", "TG5","100000" ,"10"));
+
+    }
+
+    
+    ArrayList<Sach> getList() {
+        return list;
+    }
+
+    Boolean them(Sach sach) {
+        list.add(sach);
+        return true;
+    }
+
+    Boolean sua(int i, Sach sach) {
+        list.set(i, sach);
+        return true;
+    }
+}
---
+++ src/quanlysach/Sach.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quanlysach;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Sach {
+    private String maSach,tenSach,theLoai,tacGia,donGia,soLuong;
+
+    public Sach() {
+    }
+
+    public Sach(String maSach, String tenSach, String theLoai, String tacGia, String donGia, String soLuong) {
+        this.maSach = maSach;
+        this.tenSach = tenSach;
+        this.theLoai = theLoai;
+        this.tacGia = tacGia;
+        this.donGia = donGia;
+        this.soLuong = soLuong;
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getTheLoai() {
+        return theLoai;
+    }
+
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
+
+    public String getTacGia() {
+        return tacGia;
+    }
+
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
+
+    public String getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(String donGia) {
+        this.donGia = donGia;
+    }
+
+    public String getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(String soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    @Override
+    public String toString() {
+        return "Sach{" + "maSach=" + maSach + ", tenSach=" + tenSach + ", theLoai=" + theLoai + ", tacGia=" + tacGia + ", donGia=" + donGia + ", soLuong=" + soLuong + '}';
+    }
+    
+    
+}
