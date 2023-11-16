8760d5a0c9f8dd61bd3fde65b7b9d8d043e17adc
hiennb67 <hiennbph45717@fpt.edu.vn>
2023-11-16 13:41:59 +0700

    Hien

--- src/quanlysach/QuanLySach.java
+++ src/quanlysach/QuanLySach.java
@@ -15,15 +15,14 @@ public class QuanLySach {
     ArrayList<Sach> list = new ArrayList<>();
 
     public QuanLySach() {
-        list.add(new Sach("MS001", "TAY DU KY", "TIEU THUYET", "TG1","200000" ,"20"));
-        list.add(new Sach("MS002", "DPRAEMON", "TRUYEN", "TG2","100000" ,"10"));
-        list.add(new Sach("MS003", "THANH", "TRUYEN", "TG3", "100000","30"));
-        list.add(new Sach("MS004", "THAM LAM", "TRUYEN", "TG4","100000" ,"10"));
-        list.add(new Sach("MS005", "TU DUY", "TRUYEN", "TG5","100000" ,"10"));
+        list.add(new Sach("MS001", "TAY DU KY", "TIEU THUYET", "TG1", "200000", "20"));
+        list.add(new Sach("MS002", "DPRAEMON", "TRUYEN", "TG2", "100000", "10"));
+        list.add(new Sach("MS003", "THANH", "TRUYEN", "TG3", "100000", "30"));
+        list.add(new Sach("MS004", "THAM LAM", "TRUYEN", "TG4", "100000", "10"));
+        list.add(new Sach("MS005", "TU DUY", "TRUYEN", "TG5", "100000", "10"));
 
     }
 
-    
     ArrayList<Sach> getList() {
         return list;
     }
@@ -36,5 +35,15 @@ public class QuanLySach {
     Boolean sua(int i, Sach sach) {
         list.set(i, sach);
         return true;
+    }
+
+    ArrayList<Sach> timKiem(String ma) {
+        ArrayList<Sach> listTimKiem = new ArrayList<>();
+        for (Sach s : list) {
+            if (ma.equals(s.getMaSach())) {
+                listTimKiem.add(s);
+            }
+        }
+        return listTimKiem;
     }
 }
